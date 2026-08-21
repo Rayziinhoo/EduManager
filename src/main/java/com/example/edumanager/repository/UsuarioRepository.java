1e7e831da2f40e778c11d566e2eef310d54b2b7c
Rayrison <rayoficial245@gmail.com>
2026-08-21 19:16:21 -0300

    kkkk

--- src/main/java/com/example/edumanager/repository/UsuarioRepository.java
+++ src/main/java/com/example/edumanager/repository/UsuarioRepository.java
@@ -3,5 +3,6 @@ package com.example.edumanager.repository;
 import com.example.edumanager.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
