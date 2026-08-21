7dc0d7e5779debe9e0a2e20f2b2759caddccefae
Rayrison <rayoficial245@gmail.com>
2026-08-21 19:29:16 -0300

    fak6.0

--- src/main/java/com/example/edumanager/controllers/AlunoController.java
+++ src/main/java/com/example/edumanager/controllers/AlunoController.java
@@ -1,4 +1,30 @@
 package com.example.edumanager.controllers;
 
+import com.example.edumanager.entities.Aluno;
+import com.example.edumanager.entities.Usuario;
+import com.example.edumanager.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping
+    public ResponseEntity<?> listarTodos(){
+
+        return ResponseEntity.ok(alunoRepository.findAll());
+    }
+
+    @PostMapping
+    @ResponseStatus
+    public ResponseEntity<Aluno> criar ( @RequestBody Aluno aluno) {
+        var alunoBanco = alunoRepository.save(aluno);
+        return ResponseEntity.ok(alunoBanco);
+    }
+
 }
