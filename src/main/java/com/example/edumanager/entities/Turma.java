7cbc6180ef145472ade4758742103ce411cca09e
unknown <rayoficial245@gmail.com>
2026-08-21 19:10:18 -0300

    fak4.0

--- src/main/java/com/example/edumanager/entities/Turma.java
+++ src/main/java/com/example/edumanager/entities/Turma.java
@@ -1,4 +1,20 @@
 package com.example.edumanager.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Turma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 }
