3fce1a211cdde37639fb3cb44617aa75795f1f4e
rayrison <rayoficial245@gmail.com>
2026-08-27 20:09:02 -0300

    fak12.0

--- src/main/java/com/example/edumanager/configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/edumanager/configuration/SwaggerConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.edumanager.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,16 @@ public class SwaggerConfiguration {
     public OpenAPI customOpenAPI(){
 
         return new OpenAPI()
-                .info(new Info().title("EduManager").version("1.0").description("API para currículo!"));
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .info(new Info().title("EduManager")
+                        .version("1.0")
+                        .description("API EduManager"));
 
     }
 }
