5c6ca91f5fbb2297e1a4d608071aba5bd3788b21
rayrison <rayoficial245@gmail.com>
2026-08-31 20:26:23 -0300

    fak16.0

--- src/main/java/com/example/edumanager/configuration/JwtFilter.java
+++ src/main/java/com/example/edumanager/configuration/JwtFilter.java
@@ -31,6 +31,7 @@ public class JwtFilter extends OncePerRequestFilter {
                 || uri.startsWith("webjars")
                 || uri.startsWith("/auth/login")
                 || uri.startsWith("/usuarios/cadastro")
+                || uri.startsWith("/recuperacao/conta")
 
         ){
             filterChain.doFilter(request,response);
