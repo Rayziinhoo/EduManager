5e74b59346a118dcd58f7850acf66e5f86dc1d48
Rayrison <rayoficial245@gmail.com>
2026-09-18 23:23:13 -0300

    front Fak26.0 home

--- front/app/components/Header.tsx
+++ front/app/components/Header.tsx
@@ -1,15 +1,25 @@
+import Link from "next/link";
+
 export default function Header () {
 
     return(
         <header className="bg-white border-b border-blue-900/20 shadow-sm">
             <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
                 <div className="w-10 h-10 bg-purple-900 rounded-xl flex items-center justify-center text-cyan-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
+                    <Link href="/home">
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        strokeWidth="2"  
+                    strokeLinejoin="round" 
+                    className="w-6 h-6">
                         <path d="M12 3 2 8l10 5 10-5-10-5Z"/>
                         <path d="M6 10.5V16c0 1 2.7 2.5 6 2.5s6-1.5 6-2.5v-5.5"/>
                         <circle cx="20" cy="14" r="1"/>
 
                     </svg>
+                    </Link>
                 </div>
                 <span className="text-sm font-medium text-purple-900 font-semibold"> Rayrison Ribeiro</span>
                 <button className="px-4 py-2 bg-orange-800 hover:bg-orange-900 text-white text-sm font-semibold rounded-lg transition-colors">Sair</button>
--- front/app/components/Sidebar.tsx
+++ front/app/components/Sidebar.tsx
@@ -10,12 +10,29 @@ export default function Sidebar(){
                 <span className="w-3 h-3 bg-cyan-400 rounded-full inline-block"></span>
                 <span>Edu Manager</span>
             </div>
+            {/* turmas<nomeTurma<alunos>> */}
+                {/* alunos<presença>||<notas> */}
             <nav className="flex flex-col space-y-2">
-                <Link href="/home" className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-semibold">Home</Link>
-                <Link href="/usuarios" className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-semibold">Usuários</Link>
-                <Link href="/turmas" className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-medium font-semibold">Turmas</Link>
-                <Link href="/notas" className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-medium font-semibold">Notas</Link>
-                <Link href="/alunos" className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-medium font-semibold">Alunos</Link>
+                <Link href="/home" 
+                className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-semibold">
+                    Home
+                    </Link>
+                <Link href="/usuarios" 
+                className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-semibold">
+                    Usuários
+                    </Link>
+                <Link href="/turmas" 
+                className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-medium font-semibold">
+                    Turmas
+                    </Link>
+                <Link href="/notas" 
+                className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-medium font-semibold">
+                    Notas
+                    </Link>
+                <Link href="/alunos" 
+                className="flex items-center px-4 py-3 text-cyan-200 hover:text-white hover:bg-purple-800 rounded-xl transition-all duration-200 font-medium font-semibold">
+                    Alunos
+                    </Link>
             </nav>
         </aside>);
 
